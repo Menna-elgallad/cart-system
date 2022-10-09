e984c0e727f97e7fdd3c6aa32298d963825b6504
menna elgallad <103152183+Menna-elgallad@users.noreply.github.com>
2022-10-10 01:16:01 +0200

    updated

--- dist/data.js
+++ dist/data.js
@@ -1,57 +1,57 @@
 "use strict";
 const products = [
-    {
-        id: 0,
-        name: "Earphone",
-        imgsrc: "../images/Earphone.jpg",
-        price: "350",
-    },
-    {
-        id: 1,
-        name: "Earphone",
-        imgsrc: "../images/Earphones.jpeg",
-        price: "480",
-    },
-    {
-        id: 2,
-        name: "Headphone",
-        imgsrc: "../images/headphones.jpg",
-        price: "500",
-    },
-    {
-        id: 3,
-        name: "Headphone",
-        imgsrc: "../images/headphones2.jpg",
-        price: "350",
-    },
-    {
-        id: 4,
-        name: "Keyboard",
-        imgsrc: "../images/keyboard.jpg",
-        price: "380",
-    },
-    {
-        id: 5,
-        name: "Keyboard",
-        imgsrc: "../images/keyboard2.jpg",
-        price: "280",
-    },
-    {
-        id: 6,
-        name: "Mouse",
-        imgsrc: "../images/mouse.jpg",
-        price: "250",
-    },
-    {
-        id: 7,
-        name: "Stand",
-        imgsrc: "../images/stand.jpg",
-        price: "290",
-    },
-    {
-        id: 8,
-        name: "Speaker",
-        imgsrc: "../images/speaker.jpg",
-        price: "550",
-    },
+  {
+    id: 0,
+    name: "Earphone",
+    imgsrc: "./images/Earphone.jpg",
+    price: "350",
+  },
+  {
+    id: 1,
+    name: "Earphone",
+    imgsrc: "./images/Earphones.jpeg",
+    price: "480",
+  },
+  {
+    id: 2,
+    name: "Headphone",
+    imgsrc: "./images/headphones.jpg",
+    price: "500",
+  },
+  {
+    id: 3,
+    name: "Headphone",
+    imgsrc: "./images/headphones2.jpg",
+    price: "350",
+  },
+  {
+    id: 4,
+    name: "Keyboard",
+    imgsrc: "./images/keyboard.jpg",
+    price: "380",
+  },
+  {
+    id: 5,
+    name: "Keyboard",
+    imgsrc: "./images/keyboard2.jpg",
+    price: "280",
+  },
+  {
+    id: 6,
+    name: "Mouse",
+    imgsrc: "./images/mouse.jpg",
+    price: "250",
+  },
+  {
+    id: 7,
+    name: "Stand",
+    imgsrc: "./images/stand.jpg",
+    price: "290",
+  },
+  {
+    id: 8,
+    name: "Speaker",
+    imgsrc: "./images/speaker.jpg",
+    price: "550",
+  },
 ];
